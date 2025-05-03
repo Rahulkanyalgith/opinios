bdac7f08e16c828ef1f246ca046f57189ee8cbe9
Rahul Kanyal <golusingh954015@gmail.com>
2025-05-03 11:50:01 +0530

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import express, { Application, Response, Request } from "express";
+import "dotenv/config";
+
+const app: Application = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
