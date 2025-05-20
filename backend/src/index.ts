716e00a2c8c831e4c95ffd16d0372ad698d72396
Rahul Kanyal <golusingh954015@gmail.com>
2025-05-20 23:52:09 +0530

    auth working

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,22 @@
 import express, { Application, Response, Request } from "express";
 import "dotenv/config";
+import path from "path";
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app: Application = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "./views"));
+
+
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  return res.render("hello");
 });
 
 app.listen(PORT, () => {
